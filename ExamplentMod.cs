eaea2fa86312d3d0d058d5d5231a24fb1ba4ac04
SaggyTarr <73220191+NotCrims0n@users.noreply.github.com>
2021-02-04 17:06:04 +0800

    Hotkey Update
    heck yeah

--- ExamplentMod.cs
+++ ExamplentMod.cs
@@ -1,8 +1,20 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ModLoader;
+using Terraria.ID;
 
 namespace ExamplentMod
 {
 	public class ExamplentMod : Mod
 	{
+		public static ModHotKey KillYourselfButton;
+		public override void Load()
+		{
+			Logger.InfoFormat("{0} examplent logging", Name);
+			SkillOneHotKey = RegisterHotKey("Kill Yourself Button", "L"); //registers hotkey in the Controls settings.
+			//keep in mind players have to assign the hotkey for it to work, you cannot do it automatically.
+		}
 	}
 }
